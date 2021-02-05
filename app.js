d31a6c872c72982f365e8c391041fd4fe113374b
Jon <59712501+jmillermedia@users.noreply.github.com>
2021-02-05 10:43:36 -0500

    basline commit
    Added basic file structure and node.

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express'); // like a PHP require
+const server = express();
+const path = require('path');
+
+// set our views directory
+server.set("views", path.join(__dirname, 'views'));
+// set the static assets director so express knows where to look
+// for css, js, images, etc. - anything static
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get('/', (req, res) => {
+    // console.log('hit the home route');
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+})
+
+server.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/contact.html'));
+})
+
+server.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/about.html'));
+})
+
+const port = process.env.PORT || 3000; // localhost:3000
+
+server.listen(port, () => {
+    console.log(`Server running at ${port}/`);
+});
